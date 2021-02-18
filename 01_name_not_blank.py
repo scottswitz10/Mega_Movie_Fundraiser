6dfd76c6164b5047178ade9c2bb74287d22b8515
scottswitz10 <78570301+scottswitz10@users.noreply.github.com>
2021-02-18 13:18:55 +1300

    partially working not blank

---
+++ 01_name_not_blank.py
@@ -0,0 +1,17 @@
+# Functions go here
+
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("sorry - this can't be blank")
+
+
+# Main Routine goes here
+name = not_blank("Name: ")
---
+++ 01_name_not_blank_v2.py
@@ -0,0 +1,21 @@
+# Functions go here
+
+
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print(error_message)
+
+
+# Main Routine goes here
+name = not_blank("Name: ",
+                 "sorry - this can't be blank, "
+                 "please enter your name")
+
+
