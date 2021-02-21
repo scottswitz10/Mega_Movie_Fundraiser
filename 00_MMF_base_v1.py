cafd2f3a092de5f5b62c0c0a1c6d61d2ffa6b864
scottswitz10 <78570301+scottswitz10@users.noreply.github.com>
2021-02-22 10:42:33 +1300

    Looping code

--- 00_MMF_base_v1.py
+++ 00_MMF_base_v1.py
@@ -3,6 +3,21 @@
 
 # functions go here
 
+# checks that ticket name is not blank
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        # If name is not blank, program continues
+        if response != "":
+            return response
+
+        # If name blank, show error (& repeat loop)
+        else:
+            print("sorry - this can't be blank")
+
 
 # ********** Main Routine **********
 
@@ -12,17 +27,18 @@
 
 # Loop to get ticket details
 
-     # Get name (can't be blank)
+    # Get name (can't be blank)
+    name = not_blank("Name: ")
 
-     # Get age (between 12 and 130
+    # Get age (between 12 and 130
 
-     # Calculate ticket price
+    # Calculate ticket price
 
-     # Loop to ask for snacks
+    # Loop to ask for snacks
 
-     # calculate snack price
+    # calculate snack price
 
-     # ask for payment method (and apply surcharde if necessary)
+    # ask for payment method (and apply surcharde if necessary)
 
 
 # Calculate Total sales and profit
