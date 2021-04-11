9263df2da289abd98f41dee87bf251faeff83244
scottswitz10 <78570301+scottswitz10@users.noreply.github.com>
2021-04-12 10:32:46 +1200

    partial stringcheck_v6

--- 06_string_check_v6.py
+++ 06_string_check_v6.py
@@ -3,4 +3,17 @@ import re
 
 
 # Function goes here
-def string_check
+def string_check(choice, options):
+ 
+ for var_list in options:
+ 
+ # if the snack is one of the lists, return the filter
+     if choice in var_list:
+ 
+ # get full name of snack and put it
+ # in title case so it looks nice when outputted
+        chosen = var_list[0].title()
+        print(chosen)
+        is_valid = "yes"
+        break
+        
