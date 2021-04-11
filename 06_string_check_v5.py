f1f8f39efbce83a9671a6f3e2d2835855845d6a8
scottswitz10 <78570301+scottswitz10@users.noreply.github.com>
2021-04-12 10:38:35 +1200

    working stringcheck v5

--- 06_string_check_v5.py
+++ 06_string_check_v5.py
@@ -3,49 +3,49 @@ import re
 # function goes here
 def string_check(choice, options):
  
- for var_list in options:
- 
- # if the snack is one of the lists, return the filter
-     if choice in var_list:
- 
- # get full name of snack and put it
- # in title case so it looks nice when outputted
-        chosen = var_list[0].title()
-        print(chosen)
-        is_valid = "yes"
-        break
-        
- # if the chosen option is not valid, set is_valid to no
- else:
-    is_valid = "no"
-    
-# if the snack is not OK - ask question again.
- if is_valid == "yes":
+  for var_list in options:
+
+    # if the snack is one of the lists, return the filter
+    if choice in var_list:
+
+      # get full name of snack and put it
+      # in title case so it looks nice when outputted
+      chosen = var_list[0].title()
+      print(chosen)
+      is_valid = "yes"
+      break
+
+    # if the chosen option is not valid, set is_valid to no
+    else:
+      is_valid = "no"
+
+  # if the snack is not OK - ask question again.
+  if is_valid == "yes":
     return chosen
- else:
+  else:
     return "invalid choice"
  
 # regular expression to find if the item starts with a number
 number_regex = "^[1-9]"
- 
+
 # valid snacks hold lists of all snacks
 # each item in valid snacks is a list with
 # valid options for each snack <full name, letter code (a - e)
 # , and possible abbreviations etc
 valid_snacks = [
- ["popcorn", "p", "corn", "a"],
- ["M&M's", "m&m's", "mms", "m", "b"], # first item is M&M
- ["pita chips", "chips", "pc", "pita", "c"],
- ["water", "fiji water", "fiji", "w", "d"],
- ["xxx"]
+["popcorn", "p", "corn", "a"],
+["M&M's", "m&m's", "mms", "m", "b"], # first item is M&M
+["pita chips", "chips", "pc", "pita", "c"],
+["water", "fiji water", "fiji", "w", "d"],
+["xxx"]
 ]
- 
- # valid options for free / no questions
+
+# valid options for free / no questions
 yes_no = [
- ["yes", "y"],
- ["no", "n"]
+["yes", "y"],
+["no", "n"]
 ]
- 
+
 # holds snack order for a single user
 snack_order = []
  
